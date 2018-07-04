2dca397e191c57670dafcfb2f110b3c4d0b20f04
Oanish <niculescu.oana@yahoo.com>
2018-07-04 11:16:07 +0300

    embedded SRAM complete

--- Final_Project/RAM_Top.v
+++ Final_Project/RAM_Top.v
@@ -4,9 +4,12 @@ module RAM_Top (input MasterClock,				//the board oscillator clock
 					 input WriteEnable,				//the write operation select from key 3
 					 input OutputEnable,				//the read operation select from key 2
 					 output [13:0] DigitAddr,		//the current address shown on digits 2-3
-					 output [13:0] DigitData,		//the data at the current address shown on digits 0-1
-					 output [3:0] DT, AD,			//the data and address for the embedded SRAM
+					 output [13:0] DigitData,		//the data at the current address shown on digits 0-1					 
+					 inout [3:0] DT,					//the data for the embedded SRAM
+					 output [3:0] AD,					//the address for the embedded SRAM
 					 output reg WE, OE, CE);		//control signals for the embedded SRAM
+					 
+wire [3:0] Data_Wire;
 
 always@(posedge MasterClock)
 	if(!WriteEnable)
@@ -27,11 +30,14 @@ always@(posedge MasterClock)
 				WE <= 1;
 				OE <= 1;
 			end
+			
+assign DT = (WE==0)?Data:(OE==0)?DT:4'bz;
+assign AD = Address;
 
-BCD DUT1 (.Data(Address),
+BCD DUT1 (.Data(AD),
 			 .Segments(DigitAddr));
 
-BCD DUT2 (.Data(Data),
+BCD DUT2 (.Data(DT),
 			 .Segments(DigitData));
 
 endmodule 
